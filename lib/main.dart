0de12aeff5f94cc131712c91aa15906db0e3fb42
Zaid-Alshathly-Dev <zaid20576520@gmail.com>
2026-07-24 12:09:20 +0300

    Complete lesson 3 Consumer Example

--- lib/main.dart
+++ lib/main.dart
@@ -25,66 +25,120 @@ class MyWidget extends StatefulWidget {
   State<MyWidget> createState() => _MyWidgetState();
 }
 
-// lesson-2
-// (ChangeNotifierProvider And Consumer)
-class _MyWidgetState extends State<MyWidget> {
-  // String name = "welcome";
+// // lesson-2
+// // (ChangeNotifierProvider And Consumer)
+// class _MyWidgetState extends State<MyWidget> {
+//   // String name = "welcome";
 
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => Model(),
+//       child: Scaffold(
+//         appBar: AppBar(title: const Text('Provider')),
+//         body: Consumer<Model>(
+//           builder: (context, model, child) {
+//             return Column(
+//               children: [
+//                 Center(child: Text("${model.name}")),
+//                 const SizedBox(height: 10),
+//                 MaterialButton(
+//                   color: Colors.blue,
+//                   textColor: Colors.white,
+//                   onPressed: () {
+//                     model.ChangeName();
+//                     print(model.name);
+//                   },
+//                   child: const Text('Do something'),
+//                 ),
+//               ],
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+
+//   // Scaffold(
+//   //   appBar: AppBar(title: const Text('Provider')),
+//   //   body: Column(
+//   //     children: [
+//   //       Center(child: Text("$name")),
+//   //       const SizedBox(height: 10),
+//   //       MaterialButton(
+//   //         color: Colors.blue,
+//   //         textColor: Colors.white,
+//   //         onPressed: () {
+//   //           setState(() {
+//   //             name = "zaid";
+//   //           });
+//   //         },
+//   //         child: const Text('Do something'),
+//   //       ),
+//   //     ],
+//   //   ),
+//   // );
+// }
+
+// class Model extends ChangeNotifier {
+//   String name = "zaid";
+
+//   ChangeName() {
+//     name = "welcome";
+//     notifyListeners();
+//   }
+// }
+
+// lesson-3
+// (Consumer Example)
+class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => Model(),
+      create: (context) => ProvOne(),
       child: Scaffold(
         appBar: AppBar(title: const Text('Provider')),
-        body: Consumer<Model>(
-          builder: (context, model, child) {
-            return Column(
-              children: [
-                Center(child: Text("${model.name}")),
-                const SizedBox(height: 10),
-                MaterialButton(
-                  color: Colors.blue,
-                  textColor: Colors.white,
+        body: ListView(
+          children: <Widget>[
+            Consumer<ProvOne>(
+              builder: (context, provone, child) {
+                print("consumer text1");
+                return Text(provone.showsomething1);
+              },
+            ), // Consumer
+
+            Consumer<ProvOne>(
+              builder: (context, provone, child) {
+                print("consumer text2");
+                return Text(provone.showsomething2);
+              },
+            ), // Consumer
+
+            Consumer<ProvOne>(
+              builder: (context, provone, child) {
+                print("consumer button");
+                return ElevatedButton(
                   onPressed: () {
-                    model.ChangeName();
-                    print(model.name);
+                    provone.dosomething();
                   },
-                  child: const Text('Do something'),
-                ),
-              ],
-            );
-          },
+                  child: Text("Do Something : " + provone.showsomething1),
+                ); // ElevatedButton
+              },
+            ), // Consumer
+          ], // <Widget>[]
         ),
       ),
     );
   }
-
-  // Scaffold(
-  //   appBar: AppBar(title: const Text('Provider')),
-  //   body: Column(
-  //     children: [
-  //       Center(child: Text("$name")),
-  //       const SizedBox(height: 10),
-  //       MaterialButton(
-  //         color: Colors.blue,
-  //         textColor: Colors.white,
-  //         onPressed: () {
-  //           setState(() {
-  //             name = "zaid";
-  //           });
-  //         },
-  //         child: const Text('Do something'),
-  //       ),
-  //     ],
-  //   ),
-  // );
 }
 
-class Model extends ChangeNotifier {
-  String name = "zaid";
+class ProvOne extends ChangeNotifier {
+  String showsomething1 = "Hello 1";
+  String showsomething2 = "Hello 2";
 
-  ChangeName() {
-    name = "welcome";
+  void dosomething() {
+    showsomething1 = "Changed 1";
+    showsomething2 = "Changed 2";
     notifyListeners();
   }
 }
